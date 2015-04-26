f4b61f44ad81a14b09bc48014d80c755f084ef4e
dany1468 <dany1468@gmail.com>
2015-04-26 14:39:54 +0900

    Tidy display tweets in timelines

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,5 +1,15 @@
 class TimelinesController < ApplicationController
   def index
-    @tweets = Tweet.all
+    if user_signed_in?
+      @tweets = Tweet.in(user_id: display_users)
+    else
+      @tweets = Tweet.all
+    end
+  end
+
+  private
+
+  def display_users
+    current_user.following_users << current_user
   end
 end
