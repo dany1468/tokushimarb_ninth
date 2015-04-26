da6eaacafc06b6fedeb673ea16178f2a838f449b
dany1468 <dany1468@gmail.com>
2015-04-26 14:28:08 +0900

    Redirect timeline page after tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,6 +2,6 @@ class TweetsController < ApplicationController
   def create
     Tweet.create body: params[:tweet][:body], user: current_user
 
-    redirect_to root_url
+    redirect_to timelines_path
   end
 end
