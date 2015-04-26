505817d014b0e9acdfd89aea466f3c8580996ed7
dany1468 <dany1468@gmail.com>
2015-04-26 13:38:20 +0900

    Add tweet button
    and save tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,7 @@
 class TweetsController < ApplicationController
+  def create
+    Tweet.create body: params[:tweet][:body], user: current_user
+
+    redirect_to root_url
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'items#index'
   devise_for :users
   resources :items
+  post '/tweets' => 'tweets#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
